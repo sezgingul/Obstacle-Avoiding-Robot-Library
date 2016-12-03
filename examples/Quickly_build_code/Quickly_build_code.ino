a1b091e50d1e391a6d3b2b8e12b0f52899470d90
Sezgin GÜL <sezgingul@outlook.com.tr>
2016-12-03 14:41:17 +0200

    examples code

---
+++ examples/Quickly_build_code/Quickly_build_code.ino
@@ -0,0 +1,36 @@
+/* "RobiBot" library created by Sezgin GÜL
+ *  Writing the code of the robot that detects the obstacle quickly
+ *  /**
+ * @author     Sezgin Gül
+ * @license    This project is released under The GNU General Public License v3.0
+ * @copyright  Copyright (c) 2016 Sezgin Gül
+ * @date       December 2016
+ * @website    http://www.sezgingul.com/
+ * @blog       http://www.robimek.com/
+*/
+
+/*RobiBot Shield Default Pins
+ * Ultrasonic sensor pins : Trig = D11 ; Echo = D10
+ * Servo signal pin : D12
+ * Buzzer signal pin : D3
+ * Lef dc motor : ENA: D5 ; SIGNAL PINS : D4 AND D7
+ * Right dc motor: ENA: 5 ; SIGNAL PINS : D8 AND D9
+*/
+#include <RobiBot.h> // RobiBot library
+
+RobiBot RobiBot; // crate RobiBot
+
+void setup() {
+RobiBot.begin(9600); // RobiBot baudrate
+ RobiBot.us_sensor(); //Add ultrasonic sensor 
+ RobiBot.left_motor(); // Add left motor
+ RobiBot.right_motor(); // Add right motor
+ RobiBot.servo(); // Add servo motor
+ RobiBot.motion(RELEASE); // Motors in stop position
+}
+
+void loop() {
+
+RobiBot.check(15); // Robot start function by entering obstacle detection distance
+}
+
